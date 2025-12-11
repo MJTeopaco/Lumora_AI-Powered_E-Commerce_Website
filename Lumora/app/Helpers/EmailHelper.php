64ebbd1133db85e2ffa67bc770e266195e77dac1
MJ Teopaco <mjteopaco11@gmail.com>
2025-12-12 05:35:11 +0800

    added password to email helper and service.

--- Lumora/app/Helpers/EmailHelper.php
+++ Lumora/app/Helpers/EmailHelper.php
@@ -12,7 +12,7 @@ class EmailHelper {
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'lumora.auth@gmail.com';
-        $mail->Password   = getenv('MAIL_PASSWORD') ?: 'lftkvhebzmcuqllu'; 
+        $mail->Password   = getenv('MAIL_PASSWORD') ?: 'authlumora'; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port       = 465;
         $mail->setFrom('lumora.auth@gmail.com', 'Lumora');
